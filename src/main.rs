5f848b19ef5964c2c06f712030ef1c1d1a0d1af3
Bill Figliolini <BillFigliolini@gmail.com>
2025-04-04 15:47:09 -0400

    Initial config handling in Main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{
+    env,
+    process
+};
+
+use c_compiler::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let _config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 }
