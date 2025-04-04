4bb21b43b7188131922b3bc8f2f4e6cfa32ffbe4
Bill Figliolini <BillFigliolini@gmail.com>
2025-04-04 15:54:05 -0400

    Added run to main

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ use std::{env, process};
 use c_compiler::Config;
 
 fn main() {
-    let _config = Config::build(env::args()).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
+    if let Err(e) = c_compiler::run(config) {
+        eprintln!("Error: {}", e);
+        process::exit(1);
+    }
 }
